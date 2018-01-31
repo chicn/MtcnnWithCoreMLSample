ca7d32f9e5d6b28c9a552a28802ae5e230f0d701
Chihiro Shimamura <chihiros92@suou.waseda.jp>
2018-01-31 18:04:50 +0900

    [WIP] Enable to use mtcnn

--- CoreMLExample/ViewController.swift
+++ CoreMLExample/ViewController.swift
@@ -37,7 +37,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "MyQueue"))
         self.captureSession.addOutput(videoOutput)
         self.captureSession.startRunning()
-//        setupVision()
+        setupVision()
     }
 
     override func viewDidLayoutSubviews() {
@@ -45,49 +45,59 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         self.cameraLayer.frame = self.cameraView?.bounds ?? .zero
     }
 
-//    func setupVision() {
-//        guard let visionModel = try? VNCoreMLModel(for: Inceptionv3().model)
-//            else { fatalError("Can't load VisionML model") }
-//        let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: handleClassifications)
-//        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOptionCenterCrop
-//        self.requests = [classificationRequest]
-//    }
-//
-//    func handleClassifications(request: VNRequest, error: Error?) {
-//        guard let observations = request.results
-//            else { print("no results: \(error!)"); return }
-//        let classifications = observations[0...4]
-//            .flatMap({ $0 as? VNClassificationObservation })
-//            .filter({ $0.confidence > 0.3 })
-//            .sorted(by: { $0.confidence > $1.confidence })
-//            .map {
-//                (prediction: VNClassificationObservation) -> String in
-//                return "\(round(prediction.confidence * 100 * 100)/100)%: \(prediction.identifier)"
+    func setupVision() {
+        guard let visionModel = try? VNCoreMLModel(for: MtcnnFaceDetector().model)
+            else { fatalError("Can't load VisionML model") }
+        let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: handleClassifications)
+        //classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOptionCenterCrop
+        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop
+        self.requests = [classificationRequest]
+    }
+
+    func handleClassifications(request: VNRequest, error: Error?) {
+        guard let observations = request.results
+            else { print("no results: \(error!)"); return }
+        let classifications = observations[0...4]
+            .flatMap({ $0 as? VNClassificationObservation })
+            .filter({ $0.confidence > 0.3 })
+            .sorted(by: { $0.confidence > $1.confidence })
+            .map {
+                (prediction: VNClassificationObservation) -> String in
+                return "\(round(prediction.confidence * 100 * 100)/100)%: \(prediction.identifier)"
+        }
+//        DispatchQueue.main.async {
+//            print(classifications.joined(separator: "###"))
+//            self.classificationText.text = classifications.joined(separator: "\n")
 //        }
-////        DispatchQueue.main.async {
-////            print(classifications.joined(separator: "###"))
-////            self.classificationText.text = classifications.joined(separator: "\n")
-////        }
-//    }
-//
-//    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-//        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
-//            return
-//        }
-//        var requestOptions:[VNImageOption : Any] = [:]
-//        if let cameraIntrinsicData = CMGetAttachment(sampleBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
-//            requestOptions = [.cameraIntrinsics:cameraIntrinsicData]
-//        }
-//        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: 1, options: requestOptions)
-//        do {
-//            try imageRequestHandler.perform(self.requests)
-//        } catch {
-//            print(error)
-//        }
-//    }
+    }
+
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        var requestOptions:[VNImageOption : Any] = [:]
+        if let cameraIntrinsicData = CMGetAttachment(sampleBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
+            requestOptions = [.cameraIntrinsics:cameraIntrinsicData]
+        }
+
+        //let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: 1, options: requestOptions)
+        //let orientation = CGImagePropertyOrientation(rawValue: 1)
+        //let imageRequestHandler = VNImageRequestHandler(CVPixelBuffer: pixelBuffer, orientation: Int32(orientation.rawBalue))
+        let imageRequestHandler = VNImageRequestHandler(CVPielBuffer: pixelBuffer, options: requestOptions)
+        //Argument labels '(CVPielBuffer:, options:)' do not match any available overloads
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 }
 
+
+extension UIImage {
+
+}
